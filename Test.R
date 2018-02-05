2360e53756fca50b1db50854c29fd83fc74fbf39
mattaop <snowboard95@live.no>
2018-02-05 14:40:05 +0100

    Add files via upload

---
+++ Test.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+x = seq(from = -5, to = 5, length=100)
+y = dnorm(x = x, mean = 0, sd = 1)
+df=data.frame(x=x,y=y)
+p=ggplot(data=df,aes(x=x, y=y)) + geom_line()+ ggtitle("Standard normal pdf")
+p
+
+n = 10000
+x = rnorm(n = n, mean = 0, sd = 1)
+q=ggplot(data.frame(x=x),aes(x))+geom_histogram()
+q
