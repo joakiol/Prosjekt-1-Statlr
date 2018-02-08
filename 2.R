80943e1321bbb23592159abd641ad7d0913cc62f
Lars Espeland <lars_espeland96@live.no>
2018-02-08 17:58:17 +0100

    Førsteutkast til oppgave 2

--- 2.R
+++ 2.R
@@ -35,3 +35,23 @@ ggplot(modelB, aes(sample = .stdresid)) +
 # normality test
 library(nortest)
 ad.test(rstudent(modelB))
+
+names(data)
+new=data.frame(SEX=1,AGE=56,CURSMOKE=1,BMI=89/1.75^2,TOTCHOL=200,BPMEDS=0)
+
+#Best guess for 1/sqrt(SYSBP):
+beta = modelA$"coefficients"
+x = new
+best_guess = beta["(Intercept)"]+beta["SEX"]*x["SEX"]+beta["AGE"]*x["AGE"]+beta["CURSMOKE"]*x["CURSMOKE"]+beta["BMI"]*x["BMI"]+beta["TOTCHOL"]*x["TOTCHOL"]+beta["BPMEDS"]*x["BPMEDS"]
+best_guess
+#betab = modelB$"coefficients"
+#best_guessb = betab["(Intercept)"]+betab["SEX"]*x["SEX"]+betab["AGE"]*x["AGE"]+betab["CURSMOKE"]*x["CURSMOKE"]+betab["BMI"]*x["BMI"]+betab["TOTCHOL"]*x["TOTCHOL"]+betab["BPMEDS"]*x["BPMEDS"]
+#best_guessb
+
+predict(modelA,newdata=new,interval ="prediction",type="response",level=0.90)
+"Limits of prediction interval for SYSBP:"
+lower = 1/(predict(modelA,newdata=new,interval ="prediction",type="response",level=0.90)[2]^2)
+lower
+upper = 1/(predict(modelA,newdata=new,interval ="prediction",type="response",level=0.90)[3]^2)
+upper
+
